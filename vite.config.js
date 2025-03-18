b36a14dd925dbc29d568b0d2145ad9be8e0c37ae
toosign <kevinsoo1014@gmail.com>
2025-03-18 17:18:17 +0900

    📝Docs : Title 컴포넌트 구현

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,8 @@ export default defineConfig({
         login: '/src/components/login/login.html', // 로그인 페이지
         member: '/src/components/member/member.html', // 멤버 페이지
         button: '/src/components/button/button.html', // 버튼
-        signup: '/src/components/mypage/mypage.html' // 마이 페이지
+        signup: '/src/components/mypage/mypage.html', // 마이 페이지
+        title: '/src/components/title/title.html' // 타이틀
       },
     },
   },
