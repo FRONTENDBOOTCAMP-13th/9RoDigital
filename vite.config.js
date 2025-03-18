aaffc3dd1dfa4c82b14f3662d554c12cc2d73aec
toosign <kevinsoo1014@gmail.com>
2025-03-18 17:37:53 +0900

    📝Docs : 수정된 파일 이름에 맞게 연결 링크 수정

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
         member: '/src/components/member/member.html', // 멤버 페이지
         button: '/src/components/button/button.html', // 버튼
         signup: '/src/components/mypage/mypage.html', // 마이 페이지
-        quote: '/src/components/seaction8/seaction8.html', // 오늘의 한 문장
+        quote: '/src/components/section8/section8.html', // 오늘의 한 문장
       },
     },
   },
