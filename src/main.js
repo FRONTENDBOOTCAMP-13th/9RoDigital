61e4cdddea0847200ae0935bb5fd41778aecdf35
toosign <kevinsoo1014@gmail.com>
2025-03-20 17:47:03 +0900

    🎨Feat : mermaid cdn 주소 import

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import './style.css';
 import { initAccentButtons } from '/src/components/button/accentButtonHandler.js';
 import { initAccordions } from '/src/scripts/accordionHandler';
+import mermaid from 'https://cdn.jsdelivr.net/npm/mermaid@11/dist/mermaid.esm.min.mjs';
 
 initAccentButtons();
 initAccordions();
