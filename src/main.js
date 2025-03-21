6408b6dc1c6261ee5ab38552ade518c72a820071
koo-rogie <rn5184@naver.com>
2025-03-21 11:34:36 +0900

    🎁Modify : 생각해보니 메인 페이지에서만 있으면 되어서 테일윈드가 추적하지 않도록 변경

--- src/main.js
+++ src/main.js
@@ -1,6 +1,4 @@
 import './style.css';
 import { initAccentButtons } from '/src/components/button/accentButtonHandler.js';
-import { updateDate } from '/src/components/section3/today.js';
 
 initAccentButtons();
-initupdateDate();
