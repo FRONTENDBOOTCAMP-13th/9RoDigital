d26c706242220792cf8ace7234b935e9090c6661
toosign <kevinsoo1014@gmail.com>
2025-03-21 15:29:36 +0900

    🐛 Fix : button 관련 스크립트 동작을 안하는 문제를 해결

--- src/main.js
+++ src/main.js
@@ -4,21 +4,20 @@ import './style.css';
 const currentPath = window.location.pathname;
 
 // accentButtonHandler.js는 버튼 컴포넌트 페이지와 홈 페이지에서만 로드
-if (currentPath.includes('/src/components/button/button.html') || currentPath.includes('/src/pages/home/home.html')) {
-  import('/src/components/button/accentButtonHandler.js').then((module) => {
-    const initAccentButtons = module.default;
-    initAccentButtons();
-  });
-}
+// if (currentPath.includes('/src/components/button/button.html') || currentPath.includes('/src/pages/home/home.html')) {
+//   import('/src/components/button/accentButtonHandler.js').then((module) => {
+//     const initAccentButtons = module.default;
+//     initAccentButtons();
+//   });
+// }
 
-// initAccordions과 smoothScroll은 루트 경로의 home.html과 index.html에서만 로드
-if (currentPath === '/home.html' || currentPath === '/index.html' || currentPath === '/') {
+// initAccordions과 smoothScroll은 루트 경로의 index.html에서만 로드
+if (currentPath === '/index.html' || currentPath === '/') {
   // accordionHandler 로드
   import('/src/scripts/accordionHandler.js').then((module) => {
     const initAccordions = module.default;
     initAccordions();
   });
-
   // smoothScroll 로드
   import('/src/scripts/smoothScroll.js').then((module) => {
     const smoothScroll = module.default;
