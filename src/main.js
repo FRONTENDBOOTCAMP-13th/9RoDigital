033d5e686a85bd003d0c67e90e2bde7e6722c818
koo-rogie <rn5184@naver.com>
2025-04-12 02:47:42 +0900

    🎨Feat : 사이드 팝업 추가

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import './style.css';
 import { preventHashLinkClicks } from './scripts/hrefnotclick.js';
+import { sidePopup } from './scripts/side-popup.js';
 
-  preventHashLinkClicks();
-
+preventHashLinkClicks();
+sidePopup();
